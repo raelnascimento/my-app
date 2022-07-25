7b82b1cc311fdf116764d36c9af139f8525e00ff
raelnascimento <nascimentoisrael19@gmail.com>
2022-07-25 17:23:17 -0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { HomeComponent } from './home/home.component';
+import { ItemMenuComponent } from './menu/item-menu/item-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CabecalhoComponent,
+    CarrinhoComponent,
+    HomeComponent,
+    ItemMenuComponent
   ],
   imports: [
     BrowserModule,
